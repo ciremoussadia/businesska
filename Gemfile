5d4718dfabc109904493dea96f4d2895c914681c
Ciré DIA <ciremoussadia@gmail.com>
2019-08-25 13:34:49 +0000

    Sign in
    Adds sign in functionality.

--- Gemfile
+++ Gemfile
@@ -39,8 +39,8 @@ gem 'jbuilder', '~> 2.5'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
-gem 'haml', '~> 5.1', '>= 5.1.2'
 gem 'devise', '~> 4.7'
+gem 'haml', '~> 5.1', '>= 5.1.2'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 feature 'User Signin' do
   scenario 'with valid email and password' do
-    sign_in
+    sign_in_user
     expect(page).to have_content('Sign Out')
   end
 end
 
-def sign_in
+def sign_in_user
   user = create(:user)
-  visit sign_in_path
+  visit new_user_session_path
   fill_in 'Email', with: user.email
   fill_in 'Password', with: user.password
-  click_button 'Sign in'
+  click_button 'Log in'
 end
