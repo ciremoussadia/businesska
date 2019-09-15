f63dc75834c1f2a16c5ec0da951791b233c82215
Ciré DIA <ciremoussadia@gmail.com>
2019-09-15 16:50:35 +0000

    Refactor
    Extract Test helpers to module.

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -12,11 +12,3 @@ feature 'Dashboard' do
     expect(page).to have_content('Dashboard')
   end
 end
-
-def sign_in_user
-  user = create(:user)
-  visit new_user_session_path
-  fill_in 'Email', with: user.email
-  fill_in 'Password', with: user.password
-  click_button 'Log in'
-end
