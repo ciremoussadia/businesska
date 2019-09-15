b7fc07def06d75cb8ca0ea960ec1b3f1ad1ab6ed
Ciré DIA <ciremoussadia@gmail.com>
2019-09-15 13:50:40 +0000

    Testting
    Fixing failing spec.
    Remove unnecessay specs.

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -3,10 +3,20 @@
 require 'rails_helper'
 
 feature 'Dashboard' do
+  background do
+    sign_in_user
+  end
+
   scenario 'when visits root' do
     visit root_path
     expect(page).to have_content('Dashboard')
   end
-  scenario 'does something' do
-  end
+end
+
+def sign_in_user
+  user = create(:user)
+  visit new_user_session_path
+  fill_in 'Email', with: user.email
+  fill_in 'Password', with: user.password
+  click_button 'Log in'
 end
